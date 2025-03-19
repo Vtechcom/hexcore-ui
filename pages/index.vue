56e670d00794d3d37b41fc0ed1a37bdcfc01d2c8
aniadev9 <aniadev.99@gmail.com>
2025-03-19 17:25:10 +0700

    chore: Redirect to dashboard page on home page load

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <script lang="ts" setup></script>
 
+<script lang="ts" setup>
+  navigateTo('/dashboard')
+</script>
+
 <template>
-  <div class="mt-5">HOME</div>
-  <el-button>ahihi</el-button>
+  <div class=""></div>
 </template>
 
 <style lang="scss" scoped></style>
