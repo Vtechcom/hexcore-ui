11504b897ce96c75d88f3d8a1123dda6f9dc217d
aniadev9 <aniadev.99@gmail.com>
2025-06-16 23:20:51 +0700

    Refactor code structure for improved readability and maintainability

--- constants/chain.ts
+++ constants/chain.ts
@@ -1,11 +1,11 @@
-import { Cardano } from '@cardano-sdk/core'
+import CardanoWASM from '~/lib/hydra-wallet/utils/cardano-wasm'
 
 type ChainType = 'preprod' | 'mainnet'
 
 export const CHAIN: ChainType = 'preprod'
 export const networkInfo = {
-  networkId: Cardano.ChainIds.Preprod.networkId,
-  networkMagic: Cardano.ChainIds.Preprod.networkMagic,
+  networkId: CardanoWASM.NetworkInfo.testnet_preprod().network_id(),
+  networkMagic: CardanoWASM.NetworkInfo.testnet_preprod().protocol_magic(),
   networkName: 'preprod',
   currency: {
     decimals: 6,
